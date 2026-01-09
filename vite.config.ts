4b397c2f827da35902fdf19dc5c4d96bb9192b8f
jaekyeong413-code <jaekyeong413@gmail.com>
2026-01-09 11:40:55 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/rocket-buddy-app/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
